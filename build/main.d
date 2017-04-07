c109b4a6ed1c31abe47340990bb8bbd7ca0cbf94
msdeep14 <mandeepsinghshekhawat95@gmail.com>
2017-04-07 09:20:49 +0530

    project structure redefine

---
+++ build/main.d
@@ -0,0 +1,10 @@
+build/main.o: src/main.cpp include/declaration.h include/file_handler.h \
+ include/declaration.h include/add.h
+
+include/declaration.h:
+
+include/file_handler.h:
+
+include/declaration.h:
+
+include/add.h:
